f6f691cd4e3279786c2d7ffa692262768322f767
Henrique Gustavo Konig <henriquegustavokonig@gmail.com>
2025-10-10 13:59:31 -0300

    aa

--- Backend/server.js
+++ Backend/server.js
@@ -12,12 +12,12 @@ const saltRounds = 10;
 app.use(cors());
 app.use(express.json());
 
-// ... (Configuração do pool do banco de dados permanece a mesma) ...
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
